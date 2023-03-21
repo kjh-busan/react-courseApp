72049c7028c243cc17099652973aacd27fa7526c
kjh-busan <picsmash7@gmail.com>
2023-03-21 23:16:26 +0900

    fix: WIP render error

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,13 +26,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
         <input
-          style={{
-            borderColor: !isValid ? "red" : "black",
-            backgroundColor: !isValid ? "salmon " : "transparent",
-          }}
           type="text"
           value={enteredValue}
           placeholder={!isValid && "Plz Input"}
